15a335ed6ad81ada43e1a51e8e25f5b25c1f9e06
Umair Shah Yousafzai <nicefellow1234@gmail.com>
2018-09-20 19:37:47 +0500

    Server configured for API

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -10,12 +10,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	console.log('Connected to MongoDB Server');
 
 
-	db.collection('Todos').find().count().then((docs) => {
+	/*db.collection('Todos').find().count().then((docs) => {
 		console.log('Todos');
 		console.log(JSON.stringify(docs, undefined, 2));
 	}, (err) => {
 		console.log('Unable to fetch Todos', err);
-	});
+	});*/
+
+	/*db.collection('Users').find({name: 'Umair Shah'}).toArray().then((docs) => {
+		console.log(JSON.stringify(docs, undefined, 2));
+	});*/
 
 	/*db.collection('Todos').insertOne({
 		text: 'Something to do',
